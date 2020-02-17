ad2b14ef07891b58dc6b7e7282873e7281be2acd
Andhika Arnes Syahputra <54339978+andhikaarnes13@users.noreply.github.com>
2020-02-17 08:41:32 +0700

    Add files via upload

---
+++ 15 feb js/review.js
@@ -0,0 +1,189 @@
+const variable = 'ini string'
+const integer = 1092
+const boolean = true 
+
+const object = {
+    variable : 'ini string',
+    integer : 12091,
+    boolean  : true
+
+}
+
+const array = [{
+    nama : 'Andhika',
+    kelas : 10,
+    no_absen : 8
+}, {
+    nama : 'Afif',
+    kelas : 10,
+    no_absen : 1
+},{
+    nama : 'basyir',
+    kelas : 10,
+    no_absen : 2
+},{
+    nama : 'ubay',
+    kelas : 10,
+    no_absen : 3
+},{
+    nama : 'redy',
+    kelas : 10,
+    no_absen : 4
+}, {
+    nama : 'bagas',
+    kelas : 10,
+    no_absen : 5
+}, {
+    nama : 'alam',
+    kelas : 10,
+    no_absen : 6
+}, {
+    nama : 'aldo',
+    kelas : 10,
+    no_absen : 7
+}, {
+    nama : 'arianto',
+    kelas : 10,
+    no_absen : 9
+},{
+    nama : 'bayu aji',
+    kelas : 10,
+    no_absen : 10
+}, {
+    nama : 'david',
+    kelas : 10,
+    no_absen : 11
+},{
+    nama : 'deny',
+    kelas : 10,
+    no_absen : 12
+},{
+    nama : 'dito',
+    kelas : 10,
+    no_absen : 13
+},{
+    nama : 'dylan',
+    kelas : 10,
+    no_absen : 14
+},{
+    nama : 'ghina',
+    kelas : 10,
+    no_absen : 15
+},{
+    nama : 'giyar',
+    kelas : 10,
+    no_absen : 16
+},{
+    nama : 'ibnu',
+    kelas : 10,
+    no_absen : 17
+},{
+    nama : 'merdian',
+    kelas : 10,
+    no_absen : 18
+},{
+    nama : 'adriansyah',
+    kelas : 10,
+    no_absen : 19
+},{
+    nama : 'dzikri',
+    kelas : 10,
+    no_absen : 20
+},{
+    nama : 'andika p',
+    kelas : 10,
+    no_absen : 21
+},{
+    nama : 'dani',
+    kelas : 10,
+    no_absen : 22
+},{
+    nama : 'fajriana',
+    kelas : 10,
+    no_absen : 23
+},{
+    nama : 'gilang',
+    kelas : 10,
+    no_absen : 24
+},{
+    nama : 'ihsan zaki',
+    kelas : 10,
+    no_absen : 25
+},{
+    nama : 'haekal',
+    kelas : 10,
+    no_absen : 26
+},{
+    nama : 'nauval abu bakar',
+    kelas : 10,
+    no_absen : 27
+},{
+    nama : 'rizky priono',
+    kelas : 10,
+    no_absen : 28
+},{
+    nama : 'zaki',
+    kelas : 10,
+    no_absen : 29
+},{
+    nama : 'lala',
+    kelas : 10,
+    no_absen : 30
+},{
+    nama : 'naufal adriansyah',
+    kelas : 10,
+    no_absen : 31
+},{
+    nama : 'nito',
+    kelas : 10,
+    no_absen : 32
+},{
+    nama : 'ichan',
+    kelas : 10,
+    no_absen : 33
+},{
+    nama : 'fauzan',
+    kelas : 10,
+    no_absen : 34
+},{
+    nama : 'reyhan',
+    kelas : 10,
+    no_absen : 35
+},{
+    nama : 'wafiansyah',
+    kelas : 10,
+    no_absen : 36
+},{
+    nama : 'hartanto',
+    kelas : 10,
+    no_absen : 37
+},{
+    nama : 'maulana',
+    kelas : 10,
+    no_absen : 38
+},{
+    nama : 'rizma',
+    kelas : 10,
+    no_absen : 39
+},{
+    nama : 'rufi',
+    kelas : 10,
+    no_absen : 40
+},{
+    nama : 'widya',
+    kelas : 10,
+    no_absen : 41
+},{
+    nama : 'yuiono',
+    kelas : 10,
+    no_absen : 42
+}]
+
+
+array.map((data) => {
+    console.log(`nama : ${data.nama}`)
+    console.log(`kelas : ${data.kelas}`)
+    console.log(`no absen : ${data.no_absen}`)
+    console.log('--------------')
+})
+
